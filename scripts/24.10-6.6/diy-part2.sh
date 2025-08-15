3edc13538892c8b9ffb9b3fe66695756af53a650
CoderX <85693093+PlanetEditorX@users.noreply.github.com>
2025-08-15 09:15:40 +0800

    启动keepalived模块

--- scripts/24.10-6.6/diy-part2.sh
+++ scripts/24.10-6.6/diy-part2.sh
@@ -47,16 +47,25 @@ else
   exit 1
 fi
 
-echo ">>> 强制启用 kmod-nf-ipvs 模块"
+echo ">>> 启用 keepalived和kmod-nf-ipvs 模块"
 cat <<EOF >> .config
+CONFIG_PACKAGE_keepalived=y
+CONFIG_PACKAGE_libnl-genl200=y
+CONFIG_PACKAGE_libmagic=y
+CONFIG_PACKAGE_kmod-macvlan=y
+CONFIG_PACKAGE_libnl-route200=y
+CONFIG_PACKAGE_libnfnetlink0=y
+CONFIG_PACKAGE_libip4tc2=y
+CONFIG_PACKAGE_libip6tc2=y
+CONFIG_PACKAGE_libxtables12=y
+CONFIG_PACKAGE_libipset13=y
+CONFIG_PACKAGE_keepalived-sync=y
 CONFIG_PACKAGE_kmod-nf-ipvs=y
 CONFIG_PACKAGE_kmod-ipvs=y
 CONFIG_PACKAGE_kmod-ipvs-core=y
 CONFIG_PACKAGE_kmod-ipvs-rr=y
 CONFIG_PACKAGE_kmod-ipvs-wrr=y
 CONFIG_PACKAGE_kmod-ipvs-sh=y
-CONFIG_PACKAGE_libip4tc2=y
-CONFIG_PACKAGE_libip6tc2=y
 EOF
 
 make defconfig
